7aead459b8e72a37cc672e4e8aeca354567fdde3
SachinTadkale <sachintadkale9960@gmail.com>
2025-03-24 11:59:20 +0530

    Added routing for dashboards testing purpose (Temporary)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,5 +15,21 @@ export const routes: Routes = [
     path:'login',
     pathMatch:'full',
     loadComponent: ()=>{ return import('./Pages/login/login.component').then((m)=>m.LoginComponent);}
-  }
+  },
+  {
+    path:'grocery_dashboard',
+    pathMatch:'full',
+    loadComponent: ()=>{ return import('./Pages/Dashboards/grocery/grocery.component').then((m)=>m.GroceryComponent);}
+  },
+  {
+    path:'electronic_dashboard',
+    pathMatch:'full',
+    loadComponent: ()=>{ return import('./Pages/Dashboards/electronic/electronic.component').then((m)=>m.ElectronicComponent);}
+  },
+  {
+    path:'hardware_dashboard',
+    pathMatch:'full',
+    loadComponent: ()=>{ return import('./Pages/Dashboards/hardware/hardware.component').then((m)=>m.HardwareComponent);}
+  },
+  
 ];
